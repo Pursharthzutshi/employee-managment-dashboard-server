1929bca9cdac46534700f51961b5215159b5905b
Pursharth Zutshi <pursharthzutshi@gmail.com>
2024-06-23 18:42:23 +0530

    commit

--- index.ts
+++ index.ts
@@ -3,19 +3,11 @@ import { ApolloServer } from "apollo-server-express";
 import { PubSub } from 'graphql-subscriptions';
 const typeDefs = require("./typedefs/typeDefs")
 const resolvers = require("./resolvers/resolvers")
-// import typeDefs from "../server/typeDefs/typeDefs";
-// import resolvers from "../server/resolvers/resolvers";
 
-export const app: any = express();
+const app: any = express();
 const port = 4000;
 
 const pubsub = new PubSub()
-
-
-// app.get("/",(req:Request,res:Response)=>{
-//     res.send("Hello")
-// })
-
 
 const server = new ApolloServer({
     typeDefs,
