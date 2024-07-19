7187e1331aa035e67f81cd0cc54c9e0d1612092f
Pursharth Zutshi <pursharthzutshi@gmail.com>
2024-07-19 19:27:40 +0530

    commit

--- index.ts
+++ index.ts
@@ -1,39 +1,41 @@
-import express, { Express } from 'express';
-import { ApolloServer } from "apollo-server-express";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { createServer } from 'http';
+import { WebSocketServer } from 'ws'; 
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { execute, subscribe } from 'graphql';
+const typeDefs = require("./typedefs/typeDefs");
+const resolvers = require("./resolvers/resolvers");
 import { PubSub } from 'graphql-subscriptions';
-import { url } from 'inspector';
-const typeDefs = require("./typedefs/typeDefs")
-const resolvers = require("./resolvers/resolvers")
-const cors = require('cors');
 
-const app: any = express();
+const pubsub = new PubSub();
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+const app:any = express();
+const httpServer = createServer(app);
+const cors = require("cors")
+
+const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req, res }) => ({ req, res, pubsub }), 
+});
 app.use(cors({
     url: "https://employee-managment-dashboard-client.vercel.app",
 }))
 
-const port = 4003;
-
-const pubsub = new PubSub()
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: ({ req, res }) => ({
-        req,
-        res,
-        // secret: 
-        pubsub,
-      }),
-})
-
 async function startApolloServer() {
-    await server.start();
-    server.applyMiddleware({ app, path: "/api" });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
+
+    const wsServer = new WebSocketServer({ server: httpServer, path: apolloServer.graphqlPath });
+
+    useServer({ schema, execute, subscribe }, wsServer);
 }
 
-startApolloServer()
+startApolloServer();
 
-
-app.listen(port, () => {
-    console.log(`${port} is running`);
-})
+const port = 4004;
+httpServer.listen(port, () => {
+    console.log(`Server ready at http://localhost:${port}${apolloServer.graphqlPath}`);
+});
