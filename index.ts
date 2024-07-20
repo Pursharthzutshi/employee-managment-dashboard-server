092477ddebe6170b6e30c65f2f4180ec6f3c6f6d
Pursharth Zutshi <pursharthzutshi@gmail.com>
2024-07-21 03:41:37 +0530

    commit

--- index.ts
+++ index.ts
@@ -1,24 +1,27 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import { createServer } from 'http';
-import { WebSocketServer } from 'ws'; 
-import { useServer } from 'graphql-ws/lib/use/ws';
-import { makeExecutableSchema } from '@graphql-tools/schema';
-import { execute, subscribe } from 'graphql';
-const typeDefs = require("./typedefs/typeDefs");
-const resolvers = require("./resolvers/resolvers");
-import { PubSub } from 'graphql-subscriptions';
+
+const { ApolloServer } = require('apollo-server-express');
+const { createServer } = require('http');
+const { WebSocketServer } = require('ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { execute, subscribe } = require('graphql');
+const typeDefs = require('./typedefs/typeDefs');
+const resolvers = require('./resolvers/resolvers');
+const { PubSub } = require('graphql-subscriptions');
+
 
 const pubsub = new PubSub();
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-const app:any = express();
+const express = require("express")
+const app: any = express();
+
 const httpServer = createServer(app);
 const cors = require("cors")
 
 const apolloServer = new ApolloServer({
     schema,
-    context: ({ req, res }) => ({ req, res, pubsub }), 
+    context: ({ req, res }:{req:Express.Request,res:Express.Response}) => ({ req, res, pubsub }),
 });
 app.use(cors({
     url: "https://employee-managment-dashboard-client.vercel.app",
@@ -28,7 +31,13 @@ async function startApolloServer() {
     await apolloServer.start();
     apolloServer.applyMiddleware({ app });
 
-    const wsServer = new WebSocketServer({ server: httpServer, path: apolloServer.graphqlPath });
+    const wsServer = new WebSocketServer(
+        {
+            server: httpServer,
+            
+            path: apolloServer.graphqlPath
+        }
+    );
 
     useServer({ schema, execute, subscribe }, wsServer);
 }
